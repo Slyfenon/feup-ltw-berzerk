e9fda191e8e99b455351f58f3a45aea5f5b8b67d
g0ncal0 <goncalonunes1112@gmail.com>
2023-11-22 16:12:15 +0000

    Terminal criado

---
+++ src/main/java/com/ld04gr02/berzerk/gui/GUI.java
@@ -0,0 +1,11 @@
+package com.ld04gr02.berzerk.gui;
+
+import java.io.IOException;
+
+public interface GUI {
+    void clear();
+
+    void refresh() throws IOException;
+
+    void close() throws IOException;
+}
---
+++ src/main/java/com/ld04gr02/berzerk/gui/LanternaGUI.java
@@ -0,0 +1,49 @@
+package com.ld04gr02.berzerk.gui;
+
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class LanternaGUI implements GUI {
+    private final Screen screen;
+    public LanternaGUI(Screen screen) {
+        this.screen = screen;
+    }
+    public LanternaGUI(int width, int height) throws IOException {
+        Terminal terminal = createTerminal(width, height);
+        this.screen = createScreen(terminal);
+    }
+    private Screen createScreen(Terminal terminal) throws IOException {
+        final Screen screen;
+        screen = new TerminalScreen(terminal);
+
+        screen.setCursorPosition(null);
+        screen.startScreen();
+        screen.doResizeIfNecessary();
+        return screen;
+    }
+    private Terminal createTerminal(int width, int height) throws IOException {
+        TerminalSize terminalSize = new TerminalSize(width, height + 1);
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+        Terminal terminal = terminalFactory.createTerminal();
+        return terminal;
+    }
+    @Override
+    public void clear() {
+        screen.clear();
+    }
+
+    @Override
+    public void refresh() throws IOException {
+        screen.refresh();
+    }
+
+    @Override
+    public void close() throws IOException {
+        screen.close();
+    }
+}
