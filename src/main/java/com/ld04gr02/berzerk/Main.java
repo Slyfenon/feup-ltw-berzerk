bac5e6ccb543782c31d3e043d6fa2423a861f2fb
g0ncal0 <goncalonunes1112@gmail.com>
2023-11-22 15:56:54 +0000

    build.gradle corrigido e Main adicionada

---
+++ src/main/java/com/ld04gr02/berzerk/Main.java
@@ -0,0 +1,10 @@
+package com.ld04gr02.berzerk;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //LanternaGUI lanternaGUI = new LanternaGUI(100, 100);
+
+    }
+}
