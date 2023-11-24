b07d4ad5acbdc1c98031f7517a28e68d43b14430
g0ncal0 <goncalonunes1112@gmail.com>
2023-11-24 13:45:42 +0000

    First tests- StickMan and Maze

---
+++ src/test/java/com/ld04gr02/berzerk/model/elements/StickManTests.java
@@ -0,0 +1,41 @@
+package com.ld04gr02.berzerk.model.elements;
+
+import com.ld04gr02.berzerk.model.Position;
+import com.ld04gr02.berzerk.model.game.elements.StickMan;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class StickManTests extends Assertions {
+    private StickMan stickMan;
+
+    @BeforeEach
+    public void setUp() {
+        stickMan = new StickMan(10, 15);
+    }
+
+    @Test
+    public void StickManTest() {
+        assertEquals(stickMan.getPosition(), new Position(10, 15));
+        assertEquals(stickMan.getLives(), 3);
+    }
+
+    @Test
+    public void increaseLivesTest() {
+        assertEquals(stickMan.getLives(), 3);
+        stickMan.increaseLives();
+        assertEquals(stickMan.getLives(), 4);
+        stickMan.increaseLives();
+        assertEquals(stickMan.getLives(), 5);
+        assertFalse(stickMan.increaseLives());
+        assertEquals(stickMan.getLives(), 5);
+    }
+
+    @Test
+    public void decreaseLivesTest() {
+        assertEquals(stickMan.getLives(), 3);
+        stickMan.decreaseLives();
+        assertEquals(stickMan.getLives(), 2);
+    }
+
+}
