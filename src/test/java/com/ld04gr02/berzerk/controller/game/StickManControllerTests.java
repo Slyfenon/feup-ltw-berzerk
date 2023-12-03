3b574a07b57764b2a3908227eeced4689ea07156
g0ncal0 <goncalonunes1112@gmail.com>
2023-12-03 22:58:19 +0000

    Added some more tests for controller

--- src/test/java/com/ld04gr02/berzerk/controller/game/StickManControllerTests.java
+++ src/test/java/com/ld04gr02/berzerk/controller/game/StickManControllerTests.java
@@ -1,7 +1,6 @@
-package com.ld04gr02.berzerk.controller;
+package com.ld04gr02.berzerk.controller.game;
 
 import com.ld04gr02.berzerk.Game;
-import com.ld04gr02.berzerk.controller.game.StickManController;
 import com.ld04gr02.berzerk.model.Position;
 import com.ld04gr02.berzerk.model.game.maze.Maze;
 import com.ld04gr02.berzerk.model.game.maze.MazeRenderer;
@@ -52,5 +51,20 @@ public class StickManControllerTests extends Assertions {
         Position position = stickManController.getModel().getStickMan().getPosition();
         stickManController.update(game, ARROW_LEFT, 0);
         assertEquals(stickManController.getModel().getStickMan().getPosition(), position.getLeft());
+    }
+
+    @Test
+    public void moveStickManSpaceTest() throws IOException {
+        Position position = stickManController.getModel().getStickMan().getPosition();
+        stickManController.update(game, SPACE, 0);
+        assertEquals(stickManController.getModel().getStickMan().getPosition(), position);
+    }
+
+    @Test
+    public void moveStickManCollidedTest() throws IOException {
+        Position position = stickManController.getModel().getStickMan().getPosition();
+        stickManController.getModel().getStickMan().setCollided(true);
+        stickManController.update(game, SPACE, 0);
+        assertEquals(stickManController.getModel().getStickMan().getPosition(), new Position(30, 150));
     }
 }
